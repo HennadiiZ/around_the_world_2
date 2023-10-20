afc33a4137f8558f81ce7c1a2d918b04634d62e4
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-10-20 12:26:41 -0600

    commands added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { lazy, Suspense } from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { lazy, Suspense } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
-import { CitiesProvider } from "./contexts/CitiesContext";
-import { AuthProvider } from "./contexts/FakeAuthContext";
-import ProtectedRoute from "./pages/ProtectedRoute";
+import { CitiesProvider } from './contexts/CitiesContext';
+import { AuthProvider } from './contexts/FakeAuthContext';
+import ProtectedRoute from './pages/ProtectedRoute';
 
-import CityList from "./components/CityList";
-import CountryList from "./components/CountryList";
-import City from "./components/City";
-import Form from "./components/Form";
-import SpinnerFullPage from "./components/SpinnerFullPage";
+import CityList from './components/CityList';
+import CountryList from './components/CountryList';
+import City from './components/City';
+import Form from './components/Form';
+import SpinnerFullPage from './components/SpinnerFullPage';
 
 // import Product from "./pages/Product";
 // import Pricing from "./pages/Pricing";
@@ -18,12 +18,12 @@ import SpinnerFullPage from "./components/SpinnerFullPage";
 // import AppLayout from "./pages/AppLayout";
 // import PageNotFound from "./pages/PageNotFound";
 
-const Homepage = lazy(() => import("./pages/Homepage"));
-const Product = lazy(() => import("./pages/Product"));
-const Pricing = lazy(() => import("./pages/Pricing"));
-const Login = lazy(() => import("./pages/Login"));
-const AppLayout = lazy(() => import("./pages/AppLayout"));
-const PageNotFound = lazy(() => import("./pages/PageNotFound"));
+const Homepage = lazy(() => import('./pages/Homepage'));
+const Product = lazy(() => import('./pages/Product'));
+const Pricing = lazy(() => import('./pages/Pricing'));
+const Login = lazy(() => import('./pages/Login'));
+const AppLayout = lazy(() => import('./pages/AppLayout'));
+const PageNotFound = lazy(() => import('./pages/PageNotFound'));
 
 // dist/assets/index-59fcab9b.css   30.56 kB │ gzip:   5.14 kB
 // dist/assets/index-f7c12d89.js   572.44 kB │ gzip: 151.29 kB
@@ -36,24 +36,24 @@ function App() {
           <Suspense fallback={<SpinnerFullPage />}>
             <Routes>
               <Route index element={<Homepage />} />
-              <Route path="product" element={<Product />} />
-              <Route path="pricing" element={<Pricing />} />
-              <Route path="login" element={<Login />} />
+              <Route path='product' element={<Product />} />
+              <Route path='pricing' element={<Pricing />} />
+              <Route path='login' element={<Login />} />
               <Route
-                path="app"
+                path='app'
                 element={
                   <ProtectedRoute>
                     <AppLayout />
                   </ProtectedRoute>
                 }
               >
-                <Route index element={<Navigate replace to="cities" />} />
-                <Route path="cities" element={<CityList />} />
-                <Route path="cities/:id" element={<City />} />
-                <Route path="countries" element={<CountryList />} />
-                <Route path="form" element={<Form />} />
+                <Route index element={<Navigate replace to='cities' />} />
+                <Route path='cities' element={<CityList />} />
+                <Route path='cities/:id' element={<City />} />
+                <Route path='countries' element={<CountryList />} />
+                <Route path='form' element={<Form />} />
               </Route>
-              <Route path="*" element={<PageNotFound />} />
+              <Route path='*' element={<PageNotFound />} />
             </Routes>
           </Suspense>
         </BrowserRouter>
@@ -63,3 +63,6 @@ function App() {
 }
 
 export default App;
+
+// npm run dev
+// npm run server
